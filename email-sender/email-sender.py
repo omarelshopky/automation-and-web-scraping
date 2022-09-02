cc5f1c84ae6389411d763389b9e5bbec6ab91069
Omar Hesham <omar.elshopky202@gmail.com>
2022-09-03 00:26:57 +0200

    Email Sender | Enable attach files in the auomated emails

--- email-sender/email-sender.py
+++ email-sender/email-sender.py
@@ -1,27 +1,23 @@
 ######################################
+#          Email Sender v2           #
+#                                    #
 #     Developed By DarkKnight        #
 ######################################
 
 import pandas as pd
-import smtplib
+import yagmail
 
 
 SENDER_EMAIL = 'YOUR_EMAIL'
 SENDER_PASS = 'YOUR_PASSWORD'
 SUBJECT = 'YOUR SUBJECT LIE HERE'
 TEMPLATE_PATH = './email-template.txt'
+ATTACHMENTS_BASE_PATH = './attachments/'
 DATA_PATH = './data.csv'
 NAME_HEADER = 'Name'
 EMAIL_HEADER = 'Email'
 LINK_HEADER = 'Link'
-
-EMAIL_TEXT = f"""\
-From: {SENDER_EMAIL}
-To: TO
-Subject: {SUBJECT}
-
-BODY
-"""
+ATTACHMENT_EXTENSION = '.png'
 
 
 class EmailSender:
@@ -29,9 +25,7 @@ class EmailSender:
         self.sender_email = sender_email
 
         try:
-            self.server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            self.server.ehlo()
-            self.server.login(sender_email, sender_pass)
+            self.server = yagmail.SMTP(sender_email, sender_pass)
         except Exception as e:
             print("***Can't login to this gmail***")
             print(e)
@@ -40,32 +34,37 @@ class EmailSender:
 
     def __del__(self):
         self.server.close()
-    
+
     def getTemplate(self, template_path):
         with open(template_path, 'r') as file:
             self.template = ''.join(file.readlines())
 
-    def fillTemplate(self, name, link):
+    def getEmailText(self, name, link):
         return self.template.replace('NAME', name).replace('LINK', link)
 
-    def getEmailText(self, to, name, link):
-        body = self.fillTemplate(name, link)
-        return EMAIL_TEXT.replace('TO', to).replace('BODY', body)
+    def getAttachmentFileName(self, name):
+        return ATTACHMENTS_BASE_PATH + name + ATTACHMENT_EXTENSION
 
     def send(self, to, name, link):
         try:
-            email_text = self.getEmailText(to, name, link)
-            self.server.sendmail(self.sender_email, to, email_text)
+            self.server.send(
+                to=to,
+                subject=SUBJECT,
+                contents=self.getEmailText(name, link),
+                attachments=self.getAttachmentFileName(name)
+            )
             print(f'[+] Successfully sent to: {name}')
-        except:
-            print(f'[-] Failed sent to: {name}')
-
+        except Exception as e:
+            print(f'[-] Failed sent to: {name}\n cause: {e}\n')
 
 
 emailSender = EmailSender(SENDER_EMAIL, SENDER_PASS)
 
-data = pd.read_csv(DATA_PATH) 
+data = pd.read_csv(DATA_PATH)
 
 for i in range(len(data)):
-    emailSender.send(data[EMAIL_HEADER][i], data[NAME_HEADER][i], data[LINK_HEADER][i])
-
+    emailSender.send(
+        data[EMAIL_HEADER][i],
+        data[NAME_HEADER][i], 
+        data[LINK_HEADER][i]
+    )
